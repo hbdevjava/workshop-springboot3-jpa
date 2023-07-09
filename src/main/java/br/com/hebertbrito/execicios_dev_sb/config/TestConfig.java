19ae5e7fcf235c9147ceb0c6f87b32222364440f
Hebert Brito <hbdev1211@gmail.com>
2023-07-08 21:17:11 -0300

    Order, Instant, ISO 8601 - PART 2

--- src/main/java/br/com/hebertbrito/execicios_dev_sb/config/TestConfig.java
+++ src/main/java/br/com/hebertbrito/execicios_dev_sb/config/TestConfig.java
@@ -1,6 +1,7 @@
 package br.com.hebertbrito.execicios_dev_sb.config;
 
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import br.com.hebertbrito.execicios_dev_sb.entities.Order;
 import br.com.hebertbrito.execicios_dev_sb.entities.User;
+import br.com.hebertbrito.execicios_dev_sb.repositories.OrderRepository;
 import br.com.hebertbrito.execicios_dev_sb.repositories.UserRepository;
 
 @Configuration // -> ANOTAÇAO PARA CLASSE ESPECIFICA DE CONFIGURAÇÃO;
@@ -21,6 +24,8 @@ public class TestConfig implements CommandLineRunner {
 	 */
 	@Autowired
 	private UserRepository userRepository;
+	@Autowired
+	private OrderRepository orderRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -28,8 +33,13 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 		
-		userRepository.saveAll(Arrays.asList(u1, u2));//-> RECEBE QUALQUE OBJ QUE SEJA ITERAVEL (AQUI NO CADO UM ARRAY)
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);//NOTA: "U1" ESTA SENDO PASSADO, ASSIM FAZ A ASSOCIAÇAO ENTRE OS OBJETOS
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2); 
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1); 
+		//->  ISO 8601 FORMATO DE DATA "2019-07-22T15:21:22Z" PADRAO UTC 
 		
+		userRepository.saveAll(Arrays.asList(u1, u2));//-> RECEBE QUALQUE OBJ QUE SEJA ITERAVEL (AQUI NO CADO UM ARRAY)
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		
 	}
 
--- src/main/java/br/com/hebertbrito/execicios_dev_sb/entities/User.java
+++ src/main/java/br/com/hebertbrito/execicios_dev_sb/entities/User.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.OneToMany;
 
 
@@ -25,6 +27,7 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
 	
